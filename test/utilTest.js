9c44efb223e0786c0bd3530610643405859de6cb
Pannapur <pannapur@in-Pannapur.local>
2018-11-28 15:05:09 +0530

    Added test cases for fillRow function.

--- test/utilTest.js
+++ test/utilTest.js
@@ -1,5 +1,5 @@
 const { deepEqual } = require("assert");
-const { zipper } = require("../src/util.js");
+const { zipper, fillRow } = require("../src/util.js");
 
 describe("zipper", function() {
   let zip = zipper(["a","b","c"]);
@@ -7,5 +7,16 @@ describe("zipper", function() {
   it("it should zip elements of row using zip function ", function() {
     deepEqual(zipElement([],["1"]),[[["1"],"a"]]);
     deepEqual(zip([],["2"]),[[["2"],"a"],[["2"],"b"],[["2"],"c"]]);
+  });
+});
+
+describe("fillRow", function() {
+  it("should create row filled with DEAD of given size", function() {
+    deepEqual(fillRow(1), ["DEAD"]);
+    deepEqual(fillRow(2), ["DEAD","DEAD"]);
+  });
+
+  it("should return empty array when size is not specified", function() {
+    deepEqual(fillRow(0), []);
   });
 });
